0e620339f8274cf67dd2141e169e42c0e72e8985
FinchRelia <corentin.enon@gmail.com>
2021-11-29 10:25:00 +0100

    Added theme declaration to exit script

--- .config/scripts/i3exit.sh
+++ .config/scripts/i3exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choice=$(echo -e "Exit I3\nReboot\nShutdown" | rofi -dmenu -p "Chose action")
+choice=$(echo -e "Exit I3\nReboot\nShutdown" | rofi -theme ~/.config/rofi/Themes/gruvbox.rasi -dmenu -p "Chose action")
 case $choice in
 	"Exit I3")
 		i3-msg exit;;
